3dc4b8741327ceeff18262d5ccf4b55e8a36b15c
AmyOhMy <amylmihal@gmail.com>
2014-04-27 20:54:42 -0500

    Finished Programming Basics
    Things left to do:
    - all the styling
    - number of questions answered
    - number of questions correct/incorrect
    - don't accept submit until an answer is chosen

--- application.js
+++ application.js
@@ -2,11 +2,12 @@ $(document).ready(function () {
 
 
 //questions array
+
     var questions = [{
         questionText: "Which ingredient will NOT be found in a Manhattan?",
         choices: ["Whiskey",  "Sweet Vermouth", "Angostura Bitters", "Vodka"],
         correctAnswer: 3,
-        answerText: "Whiskey stands strong in this drink. Don't forget the cherry on top!",
+        answerText: "Whiskey, not vodka, stands strong in this drink. Don't forget the cherry on top!",
         questionValue: 0
     }, {
         questionText: "A Sazerac is a New Orleans classic made with Whiskey. What is the other key ingredient that makes it special?",
@@ -30,7 +31,7 @@ $(document).ready(function () {
         questionText: "This sweet drink is a favorite of &quot;The Dude&quot;.",
         choices: ["Colorado Bulldog", "White Russian", "Kahlua Sunrise","Dark and Stormy"],
         correctAnswer: 1,
-        answerText: "In the Big Lebowski, The Dude, preferred a White Russian made with Vodka, Kahlua, and (preferably not powdered) cream.",
+        answerText: "In the Big Lebowski, The Dude preferred a White Russian made with Vodka, Kahlua, and (preferably not powdered) cream.",
         questionValue: 4
     }];
 
@@ -39,6 +40,7 @@ $(document).ready(function () {
 var numQuestions = 0;
 var userAnswer = "";
 var i = 0;
+console.log("question " + i);
     $('#title').text(questions[i].questionText);
     $('#1stChoice').append(questions[i].choices[0]);
     $('#2ndChoice').append(questions[i].choices[1]);
@@ -47,27 +49,53 @@ var i = 0;
 
 //Check guess
 
-$("#submit").on('click',function(){
-
+$("#submit").on('click',function(event){
+    event.preventDefault();
     var userAnswer = $("input[type='radio']:checked").val();
     if (userAnswer == questions[i].correctAnswer){
         $("#feedback").text("You answered correct!");
         $("#next").show();
         $("#answerText").text(questions[i].answerText);
+         if (i === 4) {
+                $("#next").hide();
+                $("#submit").hide();
+                $("#replay").show();
+            }
     } else {
-        $("#feedback").text("You are wrong!");
+        $("#feedback").text("Nope. Try again!");
     }
 
 });
 
+//reset Questions
+var newQuestion = function(){
+    $('#title').text(questions[i].questionText);
+    $('#1stChoice').text(questions[i].choices[0]);
+    $('#2ndChoice').text(questions[i].choices[1]);
+    $('#3rdChoice').text(questions[i].choices[2]);
+    $('#4thChoice').text(questions[i].choices[3]);
+    $('input:radio').prop('checked', false);
+
+};
+
+//press Next
 $("#next").on('click', function(){
-    alert("You clicked next!");
+    $("#next").hide();
+    $("#feedback").text('');
+    $("#answerText").text('');
+   //$(questions[i].choices.length).hide();
+    i = i + 1;
+    console.log("question " + i);
+    newQuestion();
 });
 
+// Reset game. location.reload() reloads the page
+    $("#replay").on('click', function () {
+        location.reload();
+       
+    });
+
+
 
 });
-
-//$(#submit).alert("You pressed submit");
-//$(#next).alert("You pressed next");
-
 
